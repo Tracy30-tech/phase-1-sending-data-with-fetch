adff33c752d69bb94091f3c732e7d94e2a16c519
Tracy <tracy.atieno@student.moringaschool.com>
2022-12-10 03:08:49 +0300

    sending-data-with-fetch

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(userName , userEmail){
+ return fetch("http://localhost:3000/users", {
+  method: "POST",
+  headers: {
+      "Content-Type": "application/json",
+     Accept: "application/json",
+  },
+  body: JSON.stringify({
+    name: userName,
+    email:userEmail
+  }),
+})
+.then(function (response) {
+    return response.json();
+  })
+  .then(function (object) {
+    console.log(object)
+  })
+  .catch(function (error) {
+     let div = document.createElement('div')
+     div.textContent = error.message
+     document.body.appendChild(div)
+  })
+}
